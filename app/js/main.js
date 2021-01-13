57217db42018758f58561ff64c759ef62b84b9fa
qamorozov <andr.morozov96@gmail.com>
2021-01-13 19:54:25 +0200

    page 404 and about

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 $(function() {
 
-
     $(".rate-star").rateYo({
         rating: 5,
         starWidth: "12px",
@@ -20,6 +19,15 @@ $(function() {
 
     $('.header__btn-menu').on('click', function() {
         $('.header__box').toggleClass('active');
+    });
+
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
     });
 
     var mixer = mixitup('.products__inner-box');
